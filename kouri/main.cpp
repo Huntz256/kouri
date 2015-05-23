7cd4661ace2d031765a7135d8f4c355443803358
Hunter Huynh <Huntz256@gmail.com>
2015-05-23 00:55:29 -0500

    Add test board output code

--- kouri/main.cpp
+++ kouri/main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 //Program execution starts here
 int main()  {
 	cout << "Hello. My name is " << NAME << ".\n";
-	int x; 
-	cin >> x;
+	
+	BOARD_STRUCTURE board;
+
+	for (int i = 0; i < BOARD_SQUARE_COUNT; i++) {
+		board.pieces[i] = 0;
+	}
+
+	for (int i = 0; i < BOARD_SQUARE_COUNT; i++) {
+		if (i % 10 == 0) {
+			cout << '\n';
+		}
+		cout << board.pieces[i];
+	}
+	
+	int x; cin >> x;
+
 	return 0;
 }
