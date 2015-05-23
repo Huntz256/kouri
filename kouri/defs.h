f4346060243cd75d0c580e17b7b9342a83533492
Hunter Huynh <Huntz256@gmail.com>
2015-05-23 18:10:59 -0500

    Add resetBoardToEmpty(), square ids; modify input

--- kouri/defs.h
+++ kouri/defs.h
@@ -14,6 +14,33 @@
 //Define a new type called U64 as an unsigned long long integer
 typedef unsigned long long U64;
 
+//Square ids for each square on a board
+const int squareID64[BOARD_SQUARE_COUNT] = {
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
+	-1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
+	-1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
+	-1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
+	-1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
+	-1, 40, 41, 42, 43, 44, 45, 46, 47, -1,
+	-1, 48, 49, 50, 51, 52, 53, 54, 55, -1,
+	-1, 56, 57, 58, 59, 60, 61, 62, 63, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+
+const int squareID120[64] = {
+	21, 22, 23, 24, 25, 26, 27, 28,
+	31, 32, 33, 34, 35, 36, 37, 38,
+	41, 42, 43, 44, 45, 46, 47, 48,
+	51, 52, 53, 54, 55, 56, 57, 58,
+	61, 62, 63, 64, 65, 66, 67, 68,
+	71, 72, 73, 74, 75, 76, 77, 78,
+	81, 82, 83, 84, 85, 86, 87, 88,
+	91, 92, 93, 94, 95, 96, 97, 98
+};
+
 //An array of these represent a move history 
 class MoveHistory {
 	int move; //Represents a move
@@ -39,7 +66,7 @@ public:
 	void displayFullBoard(bool dispPieces); //Outputs full 10x10 board to console
 	void displayBoard(bool dispPieces); //Outputs 8x8 board to console
 	void init(bool goFirst); //Sets up pieces for a standard chess match
-
+	void resetBoardToEmpty(); //Resets the board
 } board;
 
 #endif
--- kouri/main.cpp
+++ kouri/main.cpp
@@ -8,16 +8,21 @@ using namespace std;
  * Returns a character representing a piece, given a piece number.
 */
 char numToPiece(int num) {
-unordered_map<int, char> pieceMap;
-	pieceMap[0] = ' ';
-	pieceMap[1] = 'P';  pieceMap[2] = 'p'; // P/p = black pawn / white pawn
-	pieceMap[3] = 'B';  pieceMap[4] = 'b'; // B/b = black bishop / white bishop
-	pieceMap[5] = 'N';  pieceMap[6] = 'n'; // N/n = black knight / white knight
-	pieceMap[7] = 'R';  pieceMap[8] = 'r'; // R/r = black rook / white rook
-	pieceMap[9] = 'Q';  pieceMap[10] = 'q'; // Q/q = black queen / white queen
-	pieceMap[11] = 002; pieceMap[12] = 001; // smiley faces = black king / white king
+	unordered_map<int, char> pieceMap;
+	
+	if (num >= 0) {
+		pieceMap[0] = ' ';
+		pieceMap[1] = 'P';  pieceMap[2] = 'p'; // P/p = black pawn / white pawn
+		pieceMap[3] = 'B';  pieceMap[4] = 'b'; // B/b = black bishop / white bishop
+		pieceMap[5] = 'N';  pieceMap[6] = 'n'; // N/n = black knight / white knight
+		pieceMap[7] = 'R';  pieceMap[8] = 'r'; // R/r = black rook / white rook
+		pieceMap[9] = 'Q';  pieceMap[10] = 'q'; // Q/q = black queen / white queen
+		pieceMap[11] = 002; pieceMap[12] = 001; // smiley faces = black king / white king
 
-	return pieceMap[num];
+		return pieceMap[num];
+	}
+
+	return '~';
 }
 
 void BoardStructure::displayFullBoard(bool dispPieces = true){
@@ -40,7 +45,7 @@ void BoardStructure::displayFullBoard(bool dispPieces = true){
 
 void BoardStructure::displayBoard(bool dispPieces = true){
 	for (int i = 20; i < BOARD_SQUARE_COUNT-20; i++) {
-		if (i % 10 > 0 && i % 10 < 9){
+		if (i % 10 > 0 && i % 10 < 9) {
 			if (i % 10 == 1) {
 				cout << "\n------------------------\n";
 			}
@@ -59,7 +64,7 @@ void BoardStructure::displayBoard(bool dispPieces = true){
 }
 
 void BoardStructure::init(bool goFirst) {
-	if (goFirst){
+	if (goFirst) {
 		int start[120] =  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 							0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 							0, 7, 5, 3, 11, 9, 3, 5, 7, 0,
@@ -95,6 +100,26 @@ void BoardStructure::init(bool goFirst) {
 	}
 }
 
+void BoardStructure::resetBoardToEmpty() {
+	//Set all squares on the board to the value -1 representing off board squares
+	for (int i = 0; i < BOARD_SQUARE_COUNT; i++) {
+		pieces[i] = -1;
+	}
+
+	//Set only the middle 8 x 8 squares to the value 0 representing empty squares
+	for (int i = 0; i < 64; i++) {
+		pieces[squareID120[i]] = 0;
+	}
+
+	//Clear the pawns bitboard 
+	for (int i = 0; i < 2; i++) {
+		pawns[i] = 0ULL;
+	}
+
+	//No side can castle on an empty board
+	castlePerms = 0;
+}
+
 //Program execution starts here
 int main() {
 	cout << "Hello. My name is " << NAME << ".\n";
@@ -102,15 +127,20 @@ int main() {
 	string choice;
 	do {
 		cout << "Would you like to go first? (y/n) \n";
-		cin >> choice;
+		getline(cin, choice);
 	} while (choice.compare("y") != 0 && choice.compare("n") != 0);
 	
-	if (choice.compare("y") == 0) board.init(true);
-	else board.init(false);
-		
+	board.init(choice.compare("y") == 0);
+
 	board.displayBoard();
 
-	int x; cin >> x;
+	getline(cin, choice);
+
+	board.resetBoardToEmpty();
+
+	board.displayBoard();
+
+	getline(cin, choice);
 
 	return 0;
 }
